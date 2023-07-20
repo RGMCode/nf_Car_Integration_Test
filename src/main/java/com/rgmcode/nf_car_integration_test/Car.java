e625a36628adba695bbab0fbbfe32c1c17f88214
mrg <rgmengedoth@gmail.com>
2023-07-20 11:44:44 +0200

    big bang boom

---
+++ src/main/java/com/rgmcode/nf_car_integration_test/Car.java
@@ -0,0 +1,11 @@
+package com.rgmcode.nf_car_integration_test;
+
+import lombok.Data;
+
+@Data
+public class Car {
+    private String id;
+    private String manufacturer;
+    private int tires;
+    private boolean tuv;
+}
---
+++ src/main/java/com/rgmcode/nf_car_integration_test/CarService.java
@@ -0,0 +1,27 @@
+package com.rgmcode.nf_car_integration_test;
+
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class CarService {
+
+    private CarRepository carRepository = new CarRepository();
+
+    public void addCar(Car car) {
+        carRepository.addCar(car);
+    }
+
+    public List<Car> getCars() {
+        return carRepository.getCars();
+    }
+
+    public void deleteById(String id) {
+        carRepository.deleteById(id);
+    }
+
+    public void updateById(String id, Car car) {
+        carRepository.updateById(id, car);
+    }
+}
