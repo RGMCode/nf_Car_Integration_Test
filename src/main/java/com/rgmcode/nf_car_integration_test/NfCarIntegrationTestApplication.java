a4fb94dbb84816f9ddfee5a5ebba7d7fc067c54c
mrg <rgmengedoth@gmail.com>
2023-07-20 11:32:10 +0200

    Big Bang Bum

---
+++ src/main/java/com/rgmcode/nf_car_integration_test/NfCarIntegrationTestApplication.java
@@ -0,0 +1,13 @@
+package com.rgmcode.nf_car_integration_test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NfCarIntegrationTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NfCarIntegrationTestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/rgmcode/nf_car_integration_test/NfCarIntegrationTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rgmcode.nf_car_integration_test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NfCarIntegrationTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
